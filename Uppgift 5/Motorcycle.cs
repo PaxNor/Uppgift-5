ea8819e81abe83df179d9882bd181d23b250b50d
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-31 19:32:17 +0200

    Added Motorcycle class

---
+++ Uppgift 5/Motorcycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5
+{
+    internal class Motorcycle : Vehicle
+    {
+        public enum Type { sport, offroad, classic };
+        public int TopSpeed { get; set; }
+        private Type type; 
+
+        public Motorcycle(string licensePlateNr, int wheelCount, string color, string brand, int topspeed, Type type)
+            : base(licensePlateNr, wheelCount, color, brand) {
+            TopSpeed = topspeed;
+            this.type = type;
+        }
+
+        public override string ToString() {
+            return base.ToString() + $"Top speed: {TopSpeed}, Type: {type.ToString()}";
+        }
+    }
+}
