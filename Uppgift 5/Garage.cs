47325355f354c87fa7ca2b6b6493432a08167140
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-31 16:21:42 +0200

    Add project files.

---
+++ Uppgift 5/Garage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5
+{
+    internal class Garage<T> where T : Vehicle
+    {
+        private Vehicle[] parkingSpot;
+
+        public Garage(int capacity) {
+            this.parkingSpot = new Vehicle[capacity];
+        }
+    }
+}
---
+++ Uppgift 5/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5
+{
+    internal abstract class Vehicle
+    {
+        // x. registreringsnummer, färg, antal hjul och andra egenskaper ni kan komma på.
+        public string LicensePlateNr { get; set; }
+        public int WheelCount { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+
+        public Vehicle(string licensePlateNr, int wheelCount, string color, string brand) {
+            LicensePlateNr = licensePlateNr;
+            WheelCount = wheelCount;
+            Color = color;
+            Brand = brand;
+        }
+    }
+}
