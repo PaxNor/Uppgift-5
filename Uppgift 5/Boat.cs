4a40c3826048e6b8091508c44fbcd34a7f6bddce
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-31 19:35:06 +0200

    Added Boat class

---
+++ Uppgift 5/Boat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5
+{
+    internal class Boat : Vehicle
+    {
+        public int Length { get; set; }
+
+        public Boat(string licensePlateNr, int wheelCount, string color, string brand, int length)
+            : base(licensePlateNr, wheelCount, color, brand) {
+            Length = length;
+        }
+
+        public override string ToString() {
+            return base.ToString() + $"Length: {Length}";
+        }
+    }
+}
