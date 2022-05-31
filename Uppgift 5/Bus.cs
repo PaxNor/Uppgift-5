a8be0a1fa90d5cc595b9e9e1a33f0a516f6a0a7b
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-31 19:20:09 +0200

    Added Bus class

---
+++ Uppgift 5/Bus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5
+{
+    internal class Bus : Vehicle
+    {
+        public int SeatCount { get; set; }
+        public int Weight { get; set; }
+
+        public Bus(string licensePlateNr, int wheelCount, string color, string brand, int seatCount, int weight)
+            : base(licensePlateNr, wheelCount, color, brand) {
+            SeatCount = seatCount;
+            Weight = weight;
+        }
+
+        public override string ToString() {
+            return base.ToString() + $"Seat count: {SeatCount} Weight: {Weight}";
+        }
+    }
+}
