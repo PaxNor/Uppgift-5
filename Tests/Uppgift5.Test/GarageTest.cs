bcc28039539918ae743537f1afc65b66ce4669f0
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-06-06 19:36:57 +0200

    Added tests for garage

--- Tests/Uppgift5.Test/GarageTest.cs
+++ Tests/Uppgift5.Test/GarageTest.cs
@@ -6,10 +6,8 @@ namespace Uppgift5.Test
 {
     public class GarageTest
     {
-        // Add one method per test, rename to the method to
-        // the method in garage that is being tested.
         [Fact]
-        public void AddAndFindTest() {
+        public void AddTest() {
             //-- Arrange
             Garage<Vehicle> garage = new(10);
             //Car car = new("ABC123", 4, "red", "Volvo", Car.SubType sedan);
@@ -19,7 +17,52 @@ namespace Uppgift5.Test
             garage.Add(bus);
 
             //-- Assert
-            Assert.NotNull(garage.FindVehicle("ABC123"));
+            Assert.Contains(bus, garage);
+        }
+
+        [Fact]
+        public void FreeSpaceTest() {
+            //-- Arrange
+            Garage<Vehicle> garage = new(10);
+            Boat boat = new("ABC123", 0, "white", "BoatInc", 24);
+
+            //-- Act
+            garage.Add(boat);
+
+            //-- Assert
+            Assert.Equal("9", garage.FreeSpace().ToString());
+        }
+
+        [Fact]
+        public void RemoveVehicleTest() {
+            //-- Arrange
+            Garage<Vehicle> garage = new(10);
+            Bus bus = new("ABC123", 4, "silver", "Mercedes", 12, 2000);
+            Bus? removedBus;
+
+            //-- Act
+            garage.Add(bus);
+            removedBus = (Bus?)garage.RemoveVehicle("ABC123");
+
+            //-- Assert
+            Assert.True(removedBus != null);
+            Assert.Equal(bus, removedBus);
+            Assert.Equal("10", garage.FreeSpace().ToString());
+        }
+
+        [Fact]
+        public void FindVehicle() {
+            //-- Arrange
+            Garage<Vehicle> garage = new(10);
+            Bus bus = new("ABC123", 4, "gray", "Ford", 10, 2000);
+            Bus? foundVehicle;
+
+            //-- Act
+            garage.Add(bus);
+            foundVehicle = (Bus?)garage.FindVehicle("ABC123");
+
+            //-- Assert
+            Assert.Equal(bus, foundVehicle);
         }
     }
 }
