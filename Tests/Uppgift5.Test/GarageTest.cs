bc9e583353c1808e1bc4d2e5c194211fea95c905
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-06-06 18:30:00 +0200

    Added first test

--- Tests/Uppgift5.Test/GarageTest.cs
+++ Tests/Uppgift5.Test/GarageTest.cs
@@ -9,13 +9,17 @@ namespace Uppgift5.Test
         // Add one method per test, rename to the method to
         // the method in garage that is being tested.
         [Fact]
-        public void AddTest() {
+        public void AddAndFindTest() {
             //-- Arrange
             Garage<Vehicle> garage = new(10);
+            //Car car = new("ABC123", 4, "red", "Volvo", Car.SubType sedan);
+            Bus bus = new("ABC123", 6, "white", "Scania", 80, 10000);
 
             //-- Act
+            garage.Add(bus);
 
             //-- Assert
+            Assert.NotNull(garage.FindVehicle("ABC123"));
         }
     }
 }
